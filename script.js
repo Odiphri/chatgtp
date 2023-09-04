f49ae4a4ffa1c98c64a7e1b9ef42213c2189a9e8
On board <143904598+Odiphri@users.noreply.github.com>
2023-09-04 01:28:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const loadDataFromLocalstorage = () => {
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 
     const defaultText = `<div class="default-text">
-                            <h1>ChatGPT</h1>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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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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
+                            <h1>ChatGPT</h1>
                             <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
                         </div>`
 
